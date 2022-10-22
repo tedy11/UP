3bc73b364103f1064237a2fce803d8a85eed15cb
tedy11 <115184993+tedy11@users.noreply.github.com>
2022-10-22 19:47:04 +0300

    Add files via upload

---
+++ Week 1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // TASK 1
+
+    //int a, b, c;
+    //cout << "Input first number: "<< endl;
+    //cin >> a;
+    //cout << "Input second number: " << endl;
+    //cin >> b;
+    //cout << "Input three number: " << endl;
+    //cin >> c;
+    //cout << "result: " << a + b + c << endl;
+
+    // TASK 2
+
+    //int x1, x2, y1, y2;
+    //cout << "Point 1: " << endl;
+    //cout << "x = " << endl;
+    //cin >> x1;
+    //cout << "y = " << endl;
+    //cin >> y1;
+    //cout << "Point 2: " << endl;
+    //cout << "x = " << endl;
+    //cin >> x2;
+    //cout << "y = " << endl;
+    //cin >> y2;
+    //cout << "result: " << sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) << endl;
+
+    // TASK 3
+
+    //int firstNumber, secondNumber;
+    //cout << "firstNumber: ";
+    //cin >> firstNumber;
+    //cout << "secondNumber: ";
+    //cin >> secondNumber;
+    //cout << "firstNumber: " << secondNumber << endl;
+    //cout << "secondNUmber: " << firstNumber;
+
+    // TASK 4
+
+    //int number, sum = 0;
+    //cin >> number;
+    //sum = sum + (number % 10);
+    //cout << number % 10 << endl;
+    //number = number / 10;
+    //sum = sum + (number % 10);
+    //cout << number % 10 << endl;
+    //number = number / 10;
+    //sum = sum + (number % 10);
+    //cout << number % 10 << endl;
+    //number = number / 10;
+    //cout << "sum: " << sum << endl;
+}
+
