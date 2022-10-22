3625c4ca3acf0e865f8fe9528ab31f58ea6e8991
tedy11 <115184993+tedy11@users.noreply.github.com>
2022-10-22 19:43:26 +0300

    Add files via upload

---
+++ Week 2.cpp
@@ -0,0 +1,59 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    // TASK 1
+    
+    //int a, b, c;
+    //cin >> a >> b >> c;
+    //cout << "Traicho, don't forget to buy " << a << " apples, " << b << " pears and " << c << " bananas!";
+
+    // TASK 2
+    
+    //int a, b;
+    //cin >> a >> b;
+    //cout << "sum: " << a + b << " " << "difference: " << a - b << " " << "product: " << a * b;
+
+    // TASK 3
+
+    //int seconds, minutes, hours, days;
+    //cout << "seconds: ";
+    //cin >> seconds;
+    //days = seconds / 86400;
+    //seconds = seconds % 86400;
+    //hours = seconds / 3600;
+    //seconds = seconds % 3600;
+    //minutes = seconds / 60;
+    //seconds = seconds % 60;
+    //cout << days << " days, " << hours << " hours, " << minutes << " minutes and " << seconds << " seconds ";
+
+    // TASK 4
+
+    //int number, a, b, c;
+    //cin >> number;
+    //a = number % 10;
+    //number /= 10;
+    //b = number % 10;
+    //number /= 10;
+    //c = number;
+    //cout << "The reverse number  is:6 " << a << b << c;
+
+    // TASK 5
+
+    //int a, h, k;
+    //double S, S1, P, V;
+    //cout << "Strana: ";
+    //cin >> a;
+    //cout << "Visochina: ";
+    //cin >> h;
+    //cout << "Apotema: ";
+    //cin >> k;
+    //S = a * a;
+    //P = 4 * a;
+    //V = (S * h) / 3;
+    //S1 = (P * k / 2) + S;
+    //cout << "V = " << V << endl;
+    //cout << "S = " << S1 << endl;
+}
+
