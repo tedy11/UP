44d208469a4895e91f08bf0533187f6cd3e222af
tedy11 <115184993+tedy11@users.noreply.github.com>
2022-10-25 21:48:09 +0300

    Add files via upload

---
+++ Week 3.cpp
@@ -0,0 +1,222 @@
+﻿using namespace std;
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    // TASK 1
+    
+   // int a, b;
+   // cin >> a >> b;
+   // if (a > b) {
+   //     cout << a;
+   // }
+   // else {
+   //     cout << b;
+   // }
+
+
+
+
+    // TASK 2
+
+   // int month;
+   // cout << "Input a number of a //onth: ";
+   // cin >> month;
+   // if (month < 1 || month > 12) {
+   //     cout <<"Invalid month.";
+   // }
+   // else {
+   //     cout << "Valid month.";
+   // }
+
+
+
+
+    // TASK 3
+     
+   //int x, a, b;
+   //cout << "x: ";
+   //cin >> x;
+   //cout << "a: ";
+   //cin >> a;
+   //cout << "b: ";
+   //cin >> b;
+   //if (x < a || x > b) {
+   //    cout << "Out";
+   //}
+   //else {
+   //    cout << "In";
+   //}
+
+
+
+
+    // TASK 4
+
+    //char symbol;
+    //cin >> symbol;
+    //if (symbol < 'A' && symbol > 'Z' || symbol < /'a' /&& symbol > 'z')
+    //{
+    //    cout << "It's not a letter.";
+    //}
+    //else
+    //{
+    //    cout << "It's a letter.";
+    //}
+
+
+
+
+    // TASK 5
+
+    //int a, b;
+    //cin >> a >> b;
+    //if (a > 0 )
+    //{
+    //    if (b > 0) {
+    //        cout << "Both are positive.";
+    //    }
+    //    else {
+    //        cout << "Only " << a << " is positive.";
+    //    }
+    //}
+    //else {
+    //    if (b > 0) {
+    //        cout << "Only " << b << " is positive.";
+    //    }
+    //    else {
+    //        cout << "Neither is positive.";
+    //    }
+    //}
+
+
+
+    // TASK 6
+
+    //int number;
+    //cin >> number;
+    //switch (number)
+    //{
+    //case 1: cout << "January" << endl; break;
+    //case 2: cout << "February" << endl; break;
+    //case 3: cout << "Marth" << endl; break;
+    //case 4: cout << "April" << endl; break;
+    //case 5: cout << "May" << endl; break;
+    //case 6: cout << "June" << endl; break;
+    //case 7: cout << "July" << endl; break;
+    //case 8: cout << "August" << endl; break;
+    //case 9: cout << "September" << endl; break;
+    //case 10: cout << "October" << endl; break;
+    //case 11: cout << "November" << endl; break;
+    //case 12: cout << "December" << endl; break;
+    //default: cout << "It's not a month." << endl; //break;
+    //}
+
+    
+
+
+
+     //TASK 7   НЕДОВЪРШЕНА
+
+     //int n, m, k;
+     //cout << "n:";
+     //cin >> n;
+     //cout << "m:";
+     //cin >> m;
+     //cout << "k:";
+     //cin >> k;
+     //if (k % n == 0 || k % m == 0) {
+     //    cout << "YES" << endl;
+     //}
+     //else {
+     //    cout << "NO" << endl;
+     //}
+     
+
+
+
+      //TASK 8
+
+      //int a, b, c;
+      //cout << "Input a, b, c for a*x^2 + b*x + c = /0"/ << endl;
+      //cout << "a: ";
+      //cin >> a;
+      //cout << "b: ";
+      //cin >> b;
+      //cout << "c: ";
+      //cin >> c;
+      //double D = pow(b, 2) - 4 * a * c;
+      //cout << "D = " << D << endl;
+      //double Dsqrt = sqrt(D);
+      //if (D < 0) {
+      //    cout << "It has no real roots.";
+      //}
+      //else if (D == 0) {
+      //    cout << "It has one root x = " << -b / (2 /*/ a);
+      //}
+      //else if ( D > 0) {
+      //    cout << "It has two real roots x1 = " << /(-/b + Dsqrt) / (2 * a) << " and x2 = " << //(-b - Dsqrt) / (2 * a) << endl;
+      //}
+
+
+
+
+      
+        //TASK 9    
+
+        //double amount1, amount2;
+        //string currency1, currency2;
+        //cout << "Input amounr: ";
+        //cin >> amount1;
+        //cout << "Input currency: ";
+        //cin >> currency1;
+        //cout << "Input currency to convert: ";
+        //cin >> currency2;
+        //if (currency1 == "EUR"){
+        //    if (currency2 == "BGN") {
+        //        amount2 = amount1 * 1.95;
+        //        cout << amount2 << " " <<currency2;
+        //    }
+        //    else {
+        //        cout << "It's not a valid /currency.";
+        //    }
+        //}
+
+
+
+
+
+        //TASK 10
+
+        //bool beer, rakia, ice;
+        //cin >> beer >> rakia >> ice;
+        //if (beer == true) {
+        //    cout << "Drinks home";
+        //}
+        //else if (rakia == true) {
+        //    if (ice == true) {
+        //        cout << "Drinks home";
+        //    }
+        //    else {
+        //        cout << "Goes to the store";
+        //    }
+        //}
+        //else {
+        //    cout << "Goes to the store";
+        //}
+
+
+
+
+        //TASK 11
+
+       //int n, m = 0;
+       //cout << "Input n: ";
+       //cin >> n;
+       //for (int i = 1; i <= n; i++) {
+       //    m += 2 * i;
+       //}
+       //cout << "Meters to walk: " << m;
+}
+
